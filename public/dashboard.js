1711bbc793ccd86a06b792c99420f5d0c3a928f4
Ben Radler <ben@boombotix.com>
2013-04-07 17:55:42 -0700

    cleaning up a ton of legacy code, moving map markerlist activation to map button

--- public/dashboard.js
+++ public/dashboard.js
@@ -16,12 +16,6 @@ $(function(){
         title: "Pleasantville"
       };
     },
-
-    // Toggle the `done` state of this todo item.
-    // toggle: function() {
-      // this.save({done: !this.get("done")});
-    // }
-
   });
 
   // Location Collection
@@ -36,27 +30,6 @@ $(function(){
     // Save all of the locations under the `"locations"` namespace.
     // localStorage: new Backbone.LocalStorage("todos-backbone"),
     url: '/locations'
-
-    // Filter down the list of all todo items that are finished.
-    // done: function() {
-      // return this.where({done: true});
-    // },
-
-    // Filter down the list to only todo items that are still not finished.
-    // remaining: function() {
-      // return this.without.apply(this, this.done());
-    // },
-
-    // We keep the Todos in sequential order, despite being saved by unordered
-    // GUID in the database. This generates the next order number for new items.
-    // nextOrder: function() {
-      // if (!this.length) return 1;
-      // return this.last().get('order') + 1;
-    // },
-
-    // Todos are sorted by their original insertion order.
-    // comparator: 'order'
-
   });
 
   // Create our global collection of **Locations**.
@@ -76,15 +49,19 @@ $(function(){
 
     // The DOM events specific to a location.
     events: {
-      "dblclick span"    : "edit",
       // "click span"       : "selectItem",
-      'mouseenter': 'selectItem',
-      'mouseleave': 'deselectItem',
+      'click a.map'        : 'toggleSelect',
+      // 'mouseleave'       : 'deselectItem',
+      "dblclick span"    : "edit",
       "click a.edit"     : "edit",
       "click a.destroy"  : "clear",
       "keypress .edit"   : "updateOnEnter",
       "click a.btn.done" : "close",
       // "blur .edit"      : "close",
+    },
+
+    toggleSelect: function() {
+      this.model.toggleSelect();
     },
 
     selectItem: function() {
@@ -104,7 +81,6 @@ $(function(){
 
       // close marker window if open when removing
       this.model.on("remove", this.close, this);
-
     },
 
     // Re-render the titles of the todo item.
@@ -113,16 +89,11 @@ $(function(){
       this.$el.html(this.template(this.model.toJSON()));
       // this.$el.toggleClass('done', this.model.get('done'));
       // this.input = this.$('.edit');
-      this.location_title = this.$('.edit.location-title');
+      this.location_title   = this.$('.edit.location-title');
       this.location_address = this.$('.edit.location-address');
 
       return this;
     },
-
-    // Toggle the `"done"` state of the model.
-    // toggleDone: function() {
-      // this.model.toggle();
-    // },
 
     // Switch this view into `"editing"` mode, displaying the input field.
     edit: function() {
@@ -134,6 +105,7 @@ $(function(){
 
     // Close the `"editing"` mode, saving changes to the todo.
     close: function() {
+      // this.deselect();
       var location_title = this.location_title.val();
       var location_address = this.location_address.val();
       if (!location_title || !location_address) {
@@ -173,9 +145,6 @@ $(function(){
 
     // Delegated events for creating new items, and clearing completed ones.
     events: {
-      // "keypress #new-todo":  "createOnEnter",
-      // "click #clear-completed": "clearCompleted",
-      // "click #toggle-all": "toggleAllComplete",
       "click a.new-location": "create",
       "keypress input"      : "createOnEnter",
     },
@@ -184,20 +153,12 @@ $(function(){
     // collection, when items are added or changed. Kick things off by
     // loading any preexisting todos that might be saved in *localStorage*.
     initialize: function() {
-
-      // this.input = this.$("#new-todo");
       this.location_address = this.$('#new-location-address');
       this.location_title    = this.$('#new-location-title');
-
-      // this.allCheckbox = this.$("#toggle-all")[0];
 
       this.listenTo(Locations, 'add',   this.addOne);
       this.listenTo(Locations, 'reset', this.addAll);
       this.listenTo(Locations, 'all',   this.render);
-      // this.listenTo(Locations, 'refresh', this.addAll);
-
-      // this.footer = this.$('footer');
-      // this.main = $('#main');
 
       Locations.fetch({ update: true, success:this.fetchSuccessCallback() });
     },
@@ -210,20 +171,7 @@ $(function(){
     // Re-rendering the App just means refreshing the statistics -- the rest
     // of the app doesn't change.
     render: function() {
-      // console.log("rendering appView");
-      // var done = Locations.done().length;
-      // var remaining = Locations.remaining().length;
 
-      // if (Locations.length) {
-        // this.main.show();
-        // this.footer.show();
-        //this.footer.html(this.statsTemplate({done: done, remaining: remaining}));
-      // } else {
-        // this.main.hide();
-        // this.footer.hide();
-      // }
-
-      // this.allCheckbox.checked = !remaining;
     },
 
     // Add a single todo item to the list by creating a view for it, and
@@ -267,17 +215,6 @@ $(function(){
 
     createSuccessCallback: function() {
       console.log("added a new loc");
-    },
-
-    // Clear all done todo items, destroying their models.
-    clearCompleted: function() {
-      // _.invoke(Locations.done(), 'destroy');
-      // return false;
-    },
-
-    toggleAllComplete: function () {
-      // var done = this.allCheckbox.checked;
-      // Locations.each(function (todo) { todo.save({'done': done}); });
     }
   });
 
@@ -341,11 +278,6 @@ $(function(){
     });
     markerCollectionView.render();
 
-    // Render ListView
-    var listView = new App.ListView({
-      collection: this.places
-    });
-    listView.render();
   }
 
   App.createMap = function() {
@@ -362,27 +294,15 @@ $(function(){
   $(document).ready(function() {
     App.init();
 
-    // find bars
-    $('#bars').click(function() {
-      App.places.reset(bars);
-    });
-
-    // find museums
-    $('#museums').click(function() {
-      App.places.reset(museums);
-    });
-
     // add one sample location to map and list view
     $('#addBtn').click(function() {
-      App.places.add({
-        title: 'State Capitol Building',
-        lat: 44.9543075,
-        lng: -93.102222
-      });
-    });
-
-    $('#removeBtn').click(function() {
-      App.places.remove(App.places.at(0));
+      console.log(App.places);
+      Locations.add([{
+        title: 'Example',
+        lat: 37.7714695,
+        lng: -122.4368426,
+        address: "500 haight st sf ca"
+      }]);
     });
   });
 
